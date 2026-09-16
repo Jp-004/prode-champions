8f6352614b9fbeb5fe19e247eb9cbe282c6d2d7c
Jp-004 <juanchipereno811@gmail.com>
2026-09-16 01:13:12 -0300

    Tabla de goleadores

--- app/fixture/page.tsx
+++ app/fixture/page.tsx
@@ -30,7 +30,7 @@ export default function FixturePage() {
   const [pronosticosUsuarios, setPronosticosUsuarios] = useState<PronosticoEspia[]>([]);
   const [cargandoPronosticos, setCargandoPronosticos] = useState(false);
 
-  const FECHA_LIMITE = new Date("2026-09-15T16:00:00Z");
+  const FECHA_LIMITE = new Date("2026-09-17T16:00:00Z");
   const bloqueoActivo = new Date() > FECHA_LIMITE;
 
   const mostrarNotificacion = (mensaje: string, tipo: "exito" | "error" = "exito") => {
@@ -215,7 +215,7 @@ export default function FixturePage() {
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-4 h-4">
                     <path fillRule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm.75-13a.75.75 0 0 0-1.5 0v5c0 .414.336.75.75.75h4a.75.75 0 0 0 0-1.5h-3.25V5Z" clipRule="evenodd" />
                   </svg>
-                  Cierra el 15 de Septiembre
+                  Cierra el 17 de Septiembre
                 </>
               )}
             </div>
